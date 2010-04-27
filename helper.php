95d73eea8da71d2e913055514aea1a5df38c064f
stretchyboy <martyn.eggleton@gmail.org>
2010-04-27 09:29:56 +0100

    Adds the ability to access SQLITE through PDO to the tests for the availabilty of the direct sqlite library first and falls back to PDO if needed.

--- helper.php
+++ helper.php
@@ -13,10 +13,14 @@ if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
 if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 
+if (!defined('DOKU_EXT_SQLITE')) define('DOKU_EXT_SQLITE', 'sqlite');
+if (!defined('DOKU_EXT_PDO')) define('DOKU_EXT_PDO', 'pdo');
+
+
 class helper_plugin_sqlite extends DokuWiki_Plugin {
     var $db     = null;
     var $dbname = '';
-
+    var $extension = false;
     function getInfo() {
         return confToHash(dirname(__FILE__).'plugin.info.txt');
     }
@@ -25,14 +29,35 @@ class helper_plugin_sqlite extends DokuWiki_Plugin {
      * constructor
      */
     function helper_plugin_sqlite(){
+      
+      if(!$this->extension)
+      {
         if (!extension_loaded('sqlite')) {
             $prefix = (PHP_SHLIB_SUFFIX === 'dll') ? 'php_' : '';
             if(function_exists('dl')) @dl($prefix . 'sqlite.' . PHP_SHLIB_SUFFIX);
         }
 
-        if(!function_exists('sqlite_open')){
-            msg('SQLite support missing in this PHP install - plugin will not work',-1);
+        if(function_exists('sqlite_open')){
+           $this->extension = DOKU_EXT_SQLITE;
         }
+      }
+      
+      if(!$this->extension)
+      {
+        if (!extension_loaded('pdo_sqlite')) {
+            $prefix = (PHP_SHLIB_SUFFIX === 'dll') ? 'php_' : '';
+            if(function_exists('dl')) @dl($prefix . 'pdo_sqlite.' . PHP_SHLIB_SUFFIX);
+        }
+        
+        if(class_exists('pdo')){
+            $this->extension = DOKU_EXT_PDO;
+        }
+      }
+      
+      if(!$this->extension)
+      {
+        msg('SQLite & PDO SQLite support missing in this PHP install - plugin will not work',-1);
+      }
     }
 
     /**
@@ -50,27 +75,58 @@ class helper_plugin_sqlite extends DokuWiki_Plugin {
                 return true;
             }
             // close other db
-            sqlite_close($this->db);
+            if($this->extension == DOKU_EXT_SQLITE)
+            {
+              sqlite_close($this->db);
+            }
+            else
+            {
+              $this->db->close();
+            }
             $this->db     = null;
             $this->dbname = '';
         }
 
         $this->dbname = $dbname;
-        $dbfile = $conf['metadir'].'/'.$dbname.'.sqlite';
-        $init   = (!@file_exists($dbfile) || ((int) @filesize($dbfile)) < 3);
-
-        $error='';
-        $this->db = sqlite_open($dbfile, 0666, $error);
-        if(!$this->db){
-            msg("SQLite: failed to open SQLite ".$this->dbname." database ($error)",-1);
-            return false;
+        
+        $fileextension = '.sqlite';
+        if($this->extension == DOKU_EXT_PDO)
+        {
+          $fileextension = '.sqlite3';
         }
-
-        // register our custom aggregate function
-        sqlite_create_aggregate($this->db,'group_concat',
-                                array($this,'_sqlite_group_concat_step'),
-                                array($this,'_sqlite_group_concat_finalize'), 2);
-
+        
+        $dbfile = $conf['metadir'].'/'.$dbname.$fileextension;
+        $init   = (!@file_exists($dbfile) || ((int) @filesize($dbfile)) < 3);
+        
+        if($this->extension == DOKU_EXT_SQLITE)
+        {
+          $error='';
+          $this->db = sqlite_open($dbfile, 0666, $error);
+          if(!$this->db){
+              msg("SQLite: failed to open SQLite ".$this->dbname." database ($error)",-1);
+              return false;
+          }
+  
+          // register our custom aggregate function
+          sqlite_create_aggregate($this->db,'group_concat',
+                                  array($this,'_sqlite_group_concat_step'),
+                                  array($this,'_sqlite_group_concat_finalize'), 2);
+        }
+        else
+        {
+          $dsn = 'sqlite:'.$dbfile;
+          
+          try {
+              $this->db = new PDO($dsn);
+          } catch (PDOException $e) {
+            msg("SQLite: failed to open SQLite ".$this->dbname." database (".$e->getMessage().")",-1);
+              return false;
+          }
+          $this->db->sqliteCreateAggregate('group_concat',
+                                  array($this,'_sqlite_group_concat_step'),
+                                  array($this,'_sqlite_group_concat_finalize'));
+        }
+          
         $this->_updatedb($init,$updatedir);
         return true;
     }
@@ -105,7 +161,10 @@ class helper_plugin_sqlite extends DokuWiki_Plugin {
         // in case of init, add versioning table
         if($init){
             if(!$this->_runupdatefile(dirname(__FILE__).'/db.sql',0)){
-                msg('SQLite: '.$this->dbname.' database upgrade failed for version '.$i, -1);
+                if($this->extension == DOKU_EXT_SQLITE)
+                {
+                  msg('SQLite: '.$this->dbname.' database upgrade failed for version '.$i, -1);
+                }
                 return false;
             }
         }
@@ -148,8 +207,11 @@ class helper_plugin_sqlite extends DokuWiki_Plugin {
 
             $res = $this->query("$s;");
             if ($res === false) {
+              if($this->extension == DOKU_EXT_SQLITE)
+              {
                 sqlite_query($this->db, 'ROLLBACK TRANSACTION');
                 return false;
+              }
             }
         }
 
@@ -174,12 +236,26 @@ class helper_plugin_sqlite extends DokuWiki_Plugin {
                                  WHERE tbl_name = '$table'
                                    AND type = 'table'");
 
-        if(($result === false) || (sqlite_num_rows($result)<=0)){
+        if(($result === false) || ($this->extension == DOKU_EXT_SQLITE && sqlite_num_rows($result)<=0)){
             msg("ALTER TABLE failed, no such table '".hsc($table)."'",-1);
             return false;
         }
-        $row = sqlite_fetch_array($result);
-
+        
+        if($this->extension == DOKU_EXT_SQLITE )
+        {
+          $row = sqlite_fetch_array($result);
+        }
+        else
+        {
+           $row = $result->fetch(PDO::FETCH_ASSOC);
+        }
+        
+        if($row === false){
+            msg("ALTER TABLE failed, table '".hsc($table)."' had no master data",-1);
+            return false;
+        }
+        
+        
         // prepare temporary table SQL
         $tmpname = 't'.time();
         $origsql = trim(preg_replace("/[\s]+/"," ",
@@ -375,22 +451,37 @@ class helper_plugin_sqlite extends DokuWiki_Plugin {
         }
 
         // intercept ALTER TABLE statements
+        $match = null;
         if(preg_match('/^ALTER\s+TABLE\s+([\w\.]+)\s+(.*)/i',$sql,$match)){
             return $this->_altertable($match[1],$match[2]);
         }
 
         // execute query
         $err = '';
-        $res = @sqlite_query($this->db,$sql,SQLITE_ASSOC,$err);
-        if($err){
-            msg($err.':<br /><pre>'.hsc($sql).'</pre>',-1);
-            return false;
-        }elseif(!$res){
-            msg(sqlite_error_string(sqlite_last_error($this->db)).
-                ':<br /><pre>'.hsc($sql).'</pre>',-1);
-            return false;
+        if($this->extension == DOKU_EXT_SQLITE )
+        {
+          $res = @sqlite_query($this->db,$sql,SQLITE_ASSOC,$err);
+          if($err){
+              msg($err.':<br /><pre>'.hsc($sql).'</pre>',-1);
+              return false;
+          }elseif(!$res){
+              msg(sqlite_error_string(sqlite_last_error($this->db)).
+                  ':<br /><pre>'.hsc($sql).'</pre>',-1);
+              return false;
+          }
         }
-
+        else
+        {
+          $result = false;
+          
+          $res = $this->db->query($sql);
+           
+          if(!$res){
+            $err = $this->db->errorInfo();
+              msg($err[2].':<br /><pre>'.hsc($sql).'</pre>',-1);
+              return false;
+          }
+        }
         return $res;
     }
 
@@ -399,10 +490,21 @@ class helper_plugin_sqlite extends DokuWiki_Plugin {
      */
     function res2arr($res){
         $data = array();
-        if(!sqlite_num_rows($res)) return $data;
-        sqlite_rewind($res);
-        while(($row = sqlite_fetch_array($res)) !== false){
-            $data[] = $row;
+        if($this->extension == DOKU_EXT_SQLITE )
+        {
+          if(!sqlite_num_rows($res)) return $data;
+          sqlite_rewind($res);
+          while(($row = sqlite_fetch_array($res)) !== false){
+              $data[] = $row;
+          }
+        }
+        else
+        {
+          $data = $res->fetchAll(PDO::FETCH_ASSOC);
+          if(!count(data))
+          {
+            return false;
+          }
         }
         return $data;
     }
@@ -412,10 +514,33 @@ class helper_plugin_sqlite extends DokuWiki_Plugin {
      * associative array
      */
     function res2row($res,$rownum=0){
-        if(!@sqlite_seek($res,$rownum)){
-            return false;
+        if($this->extension == DOKU_EXT_SQLITE )
+        {
+          if(!@sqlite_seek($res,$rownum)){
+              return false;
+          }
+          return sqlite_fetch_array($res);
         }
-        return sqlite_fetch_array($res);
+        else
+        {
+          //very dirty replication of the same functionality (really must look at cursors)
+          $data = array();
+          //do we need to rewind?
+          $data = $res->fetchAll(PDO::FETCH_ASSOC);
+          if(!count(data))
+          {
+            return false;
+          }
+          
+          if(!isset($data[$rownum]))
+          {
+            return false;
+          }
+          else
+          {
+            return $data[$rownum];
+          }
+        }
     }
 
     /**
@@ -423,7 +548,19 @@ class helper_plugin_sqlite extends DokuWiki_Plugin {
      */
     function res2single($res)
     {
-      return sqlite_fetch_single($res);
+      if($this->extension == DOKU_EXT_SQLITE )
+      {
+        return sqlite_fetch_single($res);
+      }
+      else
+      {
+        $data = $res->fetchAll(PDO::FETCH_NUM);
+        if(!count(data))
+        {
+          return false;
+        }
+        return $data[0][0];
+      }
     }
 
     /**
@@ -439,7 +576,14 @@ class helper_plugin_sqlite extends DokuWiki_Plugin {
      * with quotes
      */
     function quote_string($string){
+      if($this->extension == DOKU_EXT_SQLITE )
+      {
         return "'".sqlite_escape_string($string)."'";
+      }
+      else
+      {
+        return $this->db->quote($string);
+      }
     }
 
 
@@ -448,7 +592,14 @@ class helper_plugin_sqlite extends DokuWiki_Plugin {
      */
     function escape_string($str)
     {
+      if($this->extension == DOKU_EXT_SQLITE )
+      {
         return sqlite_escape_string($str);
+      }
+      else
+      {
+        return trim($this->db->quote($str), "'");
+      }
     }
     
     
@@ -457,9 +608,17 @@ class helper_plugin_sqlite extends DokuWiki_Plugin {
      *
      * @link http://devzone.zend.com/article/863-SQLite-Lean-Mean-DB-Machine
      */
-    function _sqlite_group_concat_step(&$context, $string, $separator = ',') {
-         $context['sep']    = $separator;
+    function _sqlite_group_concat_step(&$context, $rownumber, $string, $separator = ',') {
+         if(is_null($context))
+         {
+           $context = array(
+             'sep'  => $separator,
+             'data' => array()
+             );
+         }
+         
          $context['data'][] = $string;
+         return $context;
     }
 
     /**
@@ -467,9 +626,14 @@ class helper_plugin_sqlite extends DokuWiki_Plugin {
      *
      * @link http://devzone.zend.com/article/863-SQLite-Lean-Mean-DB-Machine
      */
-    function _sqlite_group_concat_finalize(&$context) {
-         $context['data'] = array_unique($context['data']);
-         return join($context['sep'],$context['data']);
+    function _sqlite_group_concat_finalize(&$context, $rownumber)
+    {
+        if(!is_array($context))
+        {
+          return null;
+        }
+        $context['data'] = array_unique($context['data']);
+        return join($context['sep'],$context['data']);
     }
 
     /**
@@ -484,7 +648,14 @@ class helper_plugin_sqlite extends DokuWiki_Plugin {
      */
     function res_fetch_array($res)
     {
-      return sqlite_fetch_array($res, SQLITE_NUM);
+      if($this->extension == DOKU_EXT_SQLITE )
+      {
+        return sqlite_fetch_array($res, SQLITE_NUM);
+      }
+      else
+      {
+        return $res->fetch(PDO::FETCH_NUM);
+      }
     }
     
     
@@ -493,7 +664,14 @@ class helper_plugin_sqlite extends DokuWiki_Plugin {
      */
     function res_fetch_assoc($res)
     {
-      return sqlite_fetch_array($res, SQLITE_ASSOC);
+      if($this->extension == DOKU_EXT_SQLITE )
+      {
+        return sqlite_fetch_array($res, SQLITE_ASSOC);
+      }
+      else
+      {
+       return $res->fetch(PDO::FETCH_ASSOC);
+      }
     }
     
     
@@ -501,7 +679,21 @@ class helper_plugin_sqlite extends DokuWiki_Plugin {
     * Count the number of records in rsult
     */
     function res2count($res) {
+      if($this->extension == DOKU_EXT_SQLITE )
+      {
         return sqlite_num_rows($res);
+      }
+      else
+      {
+        $regex = '/^SELECT\s+(?:ALL\s+|DISTINCT\s+)?(?:.*?)\s+FROM\s+(.*)$/i';
+        if (preg_match($regex, $res->queryString, $output) > 0) {
+            $stmt = $this->db->query("SELECT COUNT(*) FROM {$output[1]}", PDO::FETCH_NUM);
+
+            return $stmt->fetchColumn();
+        }
+
+        return false;
+      }
     }
     
     
@@ -510,7 +702,14 @@ class helper_plugin_sqlite extends DokuWiki_Plugin {
     */
     function countChanges($db, $res)
     {
-      return sqlite_changes($db);
+      if($this->extension == DOKU_EXT_SQLITE )
+      {
+        return sqlite_changes($db);
+      }
+      else
+      {
+        return $res->rowCount();
+      }
     }
 }
 
