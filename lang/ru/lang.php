d7f774e00a6b3fd052dcc00e5f10dd46fe8c442a
Aleksandr Selivanov <alexgearbox@gmail.com>
2013-01-23 09:35:49 +0100

    Russian translation added

---
+++ lang/ru/lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Russian language file for the SQLte Plugin
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Aleksandr Selivanov <alexgearbox@gmail.com>
+ */
+
+$lang['menu'] = 'Интерфейс SQLite';
+$lang['db']   = 'База данных';
+
+$lang['index'] = 'list indexes';
+$lang['table'] = 'list tables';
