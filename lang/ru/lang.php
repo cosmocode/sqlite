b7b987ee8a72360848b07e1d815aee6a2ed32167
Aleksandr Selivanov <alexgearbox@gmail.com>
2013-08-04 16:30:17 +0200

    translation update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -1,13 +1,14 @@
 <?php
+
 /**
- * Russian language file for the SQLte Plugin
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author     Aleksandr Selivanov <alexgearbox@gmail.com>
+ * @author Aleksandr Selivanov <alexgearbox@gmail.com>
  */
-
-$lang['menu'] = 'Интерфейс SQLite';
-$lang['db']   = 'База данных';
-
-$lang['index'] = 'list indexes';
-$lang['table'] = 'list tables';
+$lang['menu']                  = 'Интерфейс SQLite';
+$lang['db']                    = 'База данных';
+$lang['index']                 = 'list indexes';
+$lang['table']                 = 'list tables';
+$lang['rename2to3']            = 'Переименовать %s.sqlite в *.sqlite3';
+$lang['convert2to3']           = 'Конвертировать %s из форумата Sqlite2 в Sqlite3';
