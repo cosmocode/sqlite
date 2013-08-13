4f5908b01c22c8d2d1980f7f5933fddfe12a7444
Mark Prins <mprins@users.sf.net>
2013-08-13 13:10:05 +0200

    translation update

---
+++ lang/nl/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Mark Prins <mprins@users.sf.net>
+ */
+$lang['menu']                  = 'SQLite interface';
+$lang['db']                    = 'Database';
+$lang['index']                 = 'toon indexen';
+$lang['table']                 = 'toon tabellen';
+$lang['rename2to3']            = 'Hernoem %s.sqlite naar *.sqlite3';
+$lang['convert2to3']           = 'Converteer %s van Sqlite2 naar Sqlite3 formaat';
