13e64e33466c5c73144cb3ae21b8744dbfec4c93
Lars Pötter <lists@ing-poetter.de>
2013-08-17 17:05:06 +0200

    translation update

---
+++ lang/de/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Lars Pötter <lists@ing-poetter.de>
+ */
+$lang['menu']                  = 'SQLite Schnittstelle';
+$lang['db']                    = 'Datenbank';
+$lang['index']                 = 'Liste der Indexe';
+$lang['table']                 = 'Liste der Tabellen';
+$lang['rename2to3']            = '%s.sqlite zu *.sqlite3 umbenennen';
+$lang['convert2to3']           = 'Konvertiert %s vom Sqlite2 in das Sqlite3 Format';
