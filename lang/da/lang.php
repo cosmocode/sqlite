2c9c3f6f611a0ff288862522102f74b232228e8e
Soren Birk <soer9648@eucl.dk>
2013-11-12 20:56:26 +0100

    translation update

---
+++ lang/da/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Soren Birk <soer9648@eucl.dk>
+ */
+$lang['menu']                  = 'SQLite grænseflade';
+$lang['db']                    = 'Database';
+$lang['index']                 = 'list indekser';
+$lang['table']                 = 'list tabeller';
+$lang['rename2to3']            = 'Omdøb %s.sqlite til *.sqlite3';
+$lang['convert2to3']           = 'Konverter %s fra Sqlite2 til Sqlite3 format';
