dc64a0c7b3f235f3050ebba854da408eccc0963f
Nicolas Friedli <nicolas@theologique.ch>
2013-12-30 18:05:05 +0100

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,12 +1,14 @@
 <?php
+
 /**
- * French language file
- *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author Emmanuel Dupin <seedfloyd@gmail.com>
+ * @author Nicolas Friedli <nicolas@theologique.ch>
  */
-
-$lang['menu'] = 'Interface SQLite';
-$lang['db']   = 'Base de données';
-
-$lang['index'] = 'afficher les index';
-$lang['table'] = 'afficher les tables';
+$lang['menu']                  = 'Interface SQLite';
+$lang['db']                    = 'Base de données';
+$lang['index']                 = 'afficher les index';
+$lang['table']                 = 'afficher les tables';
+$lang['rename2to3']            = 'Renommer %s.sqlite en *.sqlite3';
+$lang['convert2to3']           = 'Convertir %s du format Sqlite2 au format Sqlite3';
