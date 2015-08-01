54c6c40164998c0a45bc629069c3920b62ca423a
Domingo Redal <docxml@gmail.com>
2015-08-01 12:30:30 +0200

    translation update

---
+++ lang/es/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Domingo Redal <docxml@gmail.com>
+ */
+$lang['menu']                  = 'SQLite Interfaz';
+$lang['db']                    = 'Base de datos';
+$lang['index']                 = 'Lista de índices';
+$lang['table']                 = 'Lista de tablas';
+$lang['rename2to3']            = 'Renombrar %s.sqlite a *.sqlite3';
+$lang['convert2to3']           = 'Convertir %s desde el formato de Sqlite2 a Sqlite3';
