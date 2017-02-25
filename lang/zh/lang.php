b82a54bb77a11bb91292ac0ed994df0798b40b96
kuma <kuma000@qq.com>
2017-02-25 13:50:38 +0100

    translation update

--- lang/zh/lang.php
+++ lang/zh/lang.php
@@ -2,11 +2,13 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author HL <tohelong@gmail.com>
+ * @author kuma <kuma000@qq.com>
  */
-$lang['menu']                  = 'SQLite的接口';
+$lang['menu']                  = 'SQLite 界面';
 $lang['db']                    = '数据库';
-$lang['index']                 = '列表索引';
-$lang['rename2to3']            = '更改sqlite后缀为 sqlite3';
-$lang['convert2to3']           = ' Sqlite2 转 Sqlite3格式';
+$lang['index']                 = '列出索引';
+$lang['table']                 = '列出表';
+$lang['rename2to3']            = '重命名 %s.sqlite 为 *.sqlite3';
+$lang['convert2to3']           = '转换 %s 从 Sqlite2 格式 到 Sqlite3 格式';
