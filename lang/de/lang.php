0873ee809a437abfc40d70a66cb1fa3097eb69d6
Michael Bohn <mjbohn@gmail.com>
2017-12-08 19:00:30 +0100

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -2,7 +2,8 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Michael Bohn <mjbohn@gmail.com>
  * @author Tim Weinhold <tim.weinhold@gmail.com>
  */
 $lang['menu']                  = 'SQLite Interface';
@@ -11,3 +12,7 @@ $lang['index']                 = 'Indizes anzeigen';
 $lang['table']                 = 'Tabellen anzeigen';
 $lang['rename2to3']            = 'Umbenennen von %s.sqlite zu *.sqlite3';
 $lang['convert2to3']           = 'Konvertiere %s vom Sqlite2 zum Sqlite3-Format';
+$lang['export']                = 'Datenbank exportieren';
+$lang['import']                = 'Datenbank importieren';
+$lang['import_no_file']        = 'Wählen Sie eine Datei aus';
+$lang['import_success']        = 'Datenbank erfolgreich  importiert ';
