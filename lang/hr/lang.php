38bbe3a9c53d830c111d2a3826fd2631aaa1188d
Davor Turkalj <turki.bsc@gmail.com>
2018-01-18 23:45:21 +0100

    translation update

--- lang/hr/lang.php
+++ lang/hr/lang.php
@@ -11,3 +11,7 @@ $lang['index']                 = 'prikaz indeksa';
 $lang['table']                 = 'prikaz tabela';
 $lang['rename2to3']            = 'Preimenujte %s.sqlite u *.sqlite3';
 $lang['convert2to3']           = 'Pretvorite %s iz Sqlite2 u Sqlite3 oblik';
+$lang['export']                = 'izvezi bazu';
+$lang['import']                = 'uvezi bazu';
+$lang['import_no_file']        = 'Odaberi datoteku';
+$lang['import_success']        = 'Baza uspješno uvezena';
