01036aa5b353349ce7a93c0bf44979bd62a02f5b
Kalle Sommer Nielsen <kalle@php.net>
2019-06-23 08:00:20 +0200

    translation update

--- lang/da/lang.php
+++ lang/da/lang.php
@@ -2,7 +2,8 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Kalle Sommer Nielsen <kalle@php.net>
  * @author Soren Birk <soer9648@eucl.dk>
  */
 $lang['menu']                  = 'SQLite grænseflade';
@@ -11,3 +12,7 @@ $lang['index']                 = 'list indekser';
 $lang['table']                 = 'list tabeller';
 $lang['rename2to3']            = 'Omdøb %s.sqlite til *.sqlite3';
 $lang['convert2to3']           = 'Konverter %s fra Sqlite2 til Sqlite3 format';
+$lang['export']                = 'Eksporter database';
+$lang['import']                = 'Importer database';
+$lang['import_no_file']        = 'Vælg fil';
+$lang['import_success']        = 'Database importeret med succes';
