093cf58398c11a2e20e2c525750d9c447600d47c
Schplurtz le Déboulonné <schplurtz@laposte.net>
2023-10-07 09:05:27 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -3,16 +3,26 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  * @author Nicolas Friedli <nicolas@theologique.ch>
  * @author Emmanuel Dupin <seedfloyd@gmail.com>
  */
 $lang['menu']                  = 'Interface SQLite';
 $lang['db']                    = 'Base de données';
+$lang['dbversion']             = 'Afficher la version de la base de sonnées';
 $lang['index']                 = 'afficher les index';
 $lang['table']                 = 'afficher les tables';
-$lang['rename2to3']            = 'Renommer %s.sqlite en *.sqlite3';
-$lang['convert2to3']           = 'Convertir %s du format Sqlite2 au format Sqlite3';
+$lang['btn_execute']           = 'Exécuter la requête';
 $lang['export']                = 'exporter la base de données';
 $lang['import']                = 'importer la base de données';
 $lang['import_no_file']        = 'Sélectionner un fichier';
 $lang['import_success']        = 'Base de données importée avec succès';
+$lang['none']                  = 'Rien trouvé';
+$lang['query_name']            = 'Nom de la requête';
+$lang['save_query']            = 'Sauver la requête';
+$lang['delete_query']          = 'Supprimer';
+$lang['saved_queries']         = 'Requêtes enregistrées';
+$lang['validation query_required'] = 'Il faut une commande SQL.';
+$lang['validation query_name_required'] = 'Il faut un nom de requête.';
+$lang['success query_saved']   = 'Requête sauvée.';
+$lang['success query_deleted'] = 'Requête supprimée.';
