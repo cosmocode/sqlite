a7b369874d1544fbb38f2aed7945d9ca220c9e5b
Aqua5624 <razer5624@ymail.ne.jp>
2023-12-13 04:55:12 +0100

    translation update

--- lang/ja/lang.php
+++ lang/ja/lang.php
@@ -3,16 +3,26 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Aqua5624 <razer5624@ymail.ne.jp>
  * @author HokkaidoPerson <dosankomali@yahoo.co.jp>
  * @author Hideaki SAWADA <chuno@live.jp>
  */
 $lang['menu']                  = 'SQLite 接続';
 $lang['db']                    = 'データベース';
+$lang['dbversion']             = 'データベースのバージョンを表示';
 $lang['index']                 = 'index 一覧';
 $lang['table']                 = 'table 一覧';
-$lang['rename2to3']            = '%s.sqlite から *.sqlite3 へ名前変更';
-$lang['convert2to3']           = '%s を Sqlite2 形式から Sqlite3 形式へ変換';
+$lang['btn_execute']           = 'クエリを実行';
 $lang['export']                = 'データベースをエクスポートする';
 $lang['import']                = 'データベースをインポートする';
 $lang['import_no_file']        = 'ファイルを選択';
 $lang['import_success']        = 'データベースは正常にインポートされました';
+$lang['none']                  = '何も見つかりません';
+$lang['query_name']            = 'クエリ名';
+$lang['save_query']            = 'クエリを保存';
+$lang['delete_query']          = '削除';
+$lang['saved_queries']         = '保存されているクエリ';
+$lang['validation query_required'] = 'SQLコマンドが必要です';
+$lang['validation query_name_required'] = 'クエリ名は必須です';
+$lang['success query_saved']   = 'クエリが保存されました';
+$lang['success query_deleted'] = 'クエリが削除されました';
