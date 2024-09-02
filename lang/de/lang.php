58585cf75988f093ad089f1beff2554ab46f4a4d
Zayd Benhadou <zaydbenhadou@gmail.com>
2024-09-02 09:40:45 +0200

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -3,17 +3,23 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Zayd Benhadou <zaydbenhadou@gmail.com>
  * @author Michael Bohn <mjbohn@gmail.com>
  * @author Tim Weinhold <tim.weinhold@gmail.com>
  */
 $lang['menu']                  = 'SQLite Interface';
 $lang['db']                    = 'Datenbank';
+$lang['dbversion']             = 'Zeige Datenbank Version';
 $lang['index']                 = 'Indizes anzeigen';
 $lang['table']                 = 'Tabellen anzeigen';
-$lang['rename2to3']            = 'Umbenennen von %s.sqlite zu *.sqlite3';
-$lang['convert2to3']           = 'Konvertiere %s vom Sqlite2 zum Sqlite3-Format';
+$lang['btn_execute']           = 'Abfrage ausführen';
 $lang['export']                = 'Datenbank exportieren';
 $lang['download']              = 'Datenbank-Datei herunterladen';
 $lang['import']                = 'Datenbank importieren';
 $lang['import_no_file']        = 'Wählen Sie eine Datei aus';
 $lang['import_success']        = 'Datenbank erfolgreich  importiert ';
+$lang['none']                  = 'Nichts gefunden';
+$lang['save_query']            = 'Abfrage speichern';
+$lang['delete_query']          = 'Löschen';
+$lang['saved_queries']         = 'Gespeicherte Abfragen';
+$lang['validation query_required'] = 'SQL Befehl ist erforderlich.';
