02efc00fce2688098b070c664ff5f85f35ba1650
Eduardo Mozart de Oliveira <eduardomozart182@gmail.com>
2024-11-18 22:05:06 +0100

    translation update

--- lang/pt-br/lang.php
+++ lang/pt-br/lang.php
@@ -1,19 +1,28 @@
 <?php
+
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
- * @author     Daniel Dias Rodrigues <danieldiasr@gmail.com>
+ * @author Eduardo Mozart de Oliveira <eduardomozart182@gmail.com>
+ * @author Daniel Dias Rodrigues <danieldiasr@gmail.com>
  */
-$lang['menu'] = 'Interface SQLite';
-$lang['db']   = 'Banco de dados';
-
-$lang['index'] = 'listar índices';
-$lang['table'] = 'listar tabelas';
-
-$lang['rename2to3']  = 'Renomear %s.sqlite para *.sqlite3';
-$lang['convert2to3'] = 'Converter %s do formato Sqlite2 para Sqlite3';
-
-$lang['export'] = 'exportar banco de dados';
-$lang['import'] = 'importar banco de dados';
-$lang['import_no_file'] = 'Escolha o arquivo';
-$lang['import_success'] = 'Banco de dados importado com sucesso';
+$lang['menu']                  = 'Interface SQLite';
+$lang['db']                    = 'Banco de dados';
+$lang['dbversion']             = 'Mostrar versão do banco de dados';
+$lang['index']                 = 'Listar índices';
+$lang['table']                 = 'Listar tabelas';
+$lang['btn_execute']           = 'Executar consulta';
+$lang['export']                = 'Exportar banco de dados';
+$lang['download']              = 'Baixar arquivo de banco de dados';
+$lang['import']                = 'Importar banco de dados';
+$lang['import_no_file']        = 'Escolha o arquivo';
+$lang['import_success']        = 'Banco de dados importado com sucesso';
+$lang['none']                  = 'Nada encontrado';
+$lang['query_name']            = 'Nome da consulta';
+$lang['save_query']            = 'Salvar consulta';
+$lang['delete_query']          = 'Excluir';
+$lang['saved_queries']         = 'Consultas salvas';
+$lang['validation query_required'] = 'Comando SQL é necessário.';
+$lang['validation query_name_required'] = 'O nome da consulta é obrigatório.';
+$lang['success query_saved']   = 'Consulta salva.';
+$lang['success query_deleted'] = 'Consulta excluída.';
